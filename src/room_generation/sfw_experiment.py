6e6e068a989791aca2e95a0e696a1318fb150677
tsprunck <tom.sprunck@gmail.com>
2023-02-16 16:04:00 +0100

    -bugfix: source/wall separation parameter was replaced by mike/wall separation -cleaned up

--- src/room_generation/sfw_experiment.py
+++ src/room_generation/sfw_experiment.py
@@ -1,15 +1,12 @@
 """
-Creating the parameter files for multiple random experiments and launching simulations
+Creating the parameter files for multiple random experiments.
 """
 
 import numpy as np
 from scipy.spatial.transform import Rotation
 import json
-import pandas as pd
 import os
-import sys
 from src.simulation.utils import dict_to_json
-from src.simulation.simulate_pra import load_antenna, simulate_rir
 
 tol_recov = 2e-2
 
@@ -59,13 +56,7 @@ else:  # loading the global parameters for the experiments
     fd = open(conf_path, 'r')
     param_dict = json.load(fd)
 
-df_path = os.path.join(directory, "results.csv")
-if not os.path.exists(df_path):
-    df_res = pd.DataFrame(columns=["exp_id", "min_dist", "mean_dist", "max_dist", "nb_recov"])
-else:
-    df_res = pd.read_csv(df_path)
-
-new_exp = 1000
+new_exp = 200
 n_generated = 0
 while new_exp > n_generated:
     n_generated += 1
@@ -82,10 +73,11 @@ while new_exp > n_generated:
 
     # lower and upper bounds for the random coordinates
     lb = param_dict["mic_wall_sep"]
-    ub = (xroom - param_dict["mic_wall_sep"], yroom - param_dict["mic_wall_sep"], zroom - param_dict["mic_wall_sep"])
+    ubmic = (xroom - param_dict["mic_wall_sep"], yroom - param_dict["mic_wall_sep"], zroom - param_dict["mic_wall_sep"])
+    ubsrc = (xroom - param_dict["src_wall_sep"], yroom - param_dict["src_wall_sep"], zroom - param_dict["src_wall_sep"])
     fixed_src, fixed_mic = [None, None, z_src], [None, None, z_mic]
-    args_src = [(lb, ub[i], fixed_src[i]) for i in range(3)]
-    args_mic = [(lb, ub[i], fixed_mic[i]) for i in range(3)]
+    args_src = [(lb, ubsrc[i], fixed_src[i]) for i in range(3)]
+    args_mic = [(lb, ubmic[i], fixed_mic[i]) for i in range(3)]
 
     src_pos = np.array([choose_random(*args_src[i]) for i in range(3)])
     mic_pos = np.array([choose_random(*args_mic[i]) for i in range(3)])
